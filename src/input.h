84f4c048df9adfb5ce745c6605b2d5b94ee90521
Joseiacevedo <ja3249704@gmail.com>
2022-04-13 17:58:01 -0300

    Entrega Trabajo Practico

---
+++ src/input.h
@@ -0,0 +1,14 @@
+#ifndef INPUT_H_
+#define INPUT_H_
+
+float PedirEntero(char mensaje[]);
+int ValidarOpcion(int numero);
+float ValidarEntero(char numero[]);
+float Debito(float num1);
+float Credito(float num1);
+float Bitcoin(float num1);
+float Unitario(float num1, float num2);
+int CalcularDiferencia(float num1, float num2);
+
+
+#endif
